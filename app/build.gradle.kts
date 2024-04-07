aaccb0bde4cc94030af2e3905d8998e602537426
navleen <navleensingh4@gmail.com>
2024-04-07 19:27:02 +0100

    update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,8 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth:22.3.1")
+    implementation("com.google.firebase:firebase-storage:20.3.0")
+    implementation("com.google.firebase:firebase-database:20.3.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -46,4 +48,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test:runner:1.5.2")
     testImplementation("org.robolectric:robolectric:4.6.1")
+    implementation("com.squareup.picasso:picasso:2.71828")
+
 }
